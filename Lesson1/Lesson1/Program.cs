94e6d6e090a6ab008c02d6b97a73b9651064fdb4
grimashevich <k.grimashevich@gmail.com>
2021-08-30 22:28:34 +0900

    Lesson 1

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя: ");
+            string username = Console.ReadLine();
+            DateTime today = DateTime.Today;
+            Console.WriteLine("Привет, " + username + " сегодня " + today.ToString("d MMM yyyy"));
+        }
+    }
+}
