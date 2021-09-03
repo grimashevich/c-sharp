d6c3228374493c36fbc416ece07e4cb1f9d91e09
grimashevich <k.grimashevich@gmail.com>
2021-09-03 22:18:04 +0900

    Task 2 && task 3

---
+++ Lesson3/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phonebook = new string[5, 2];
+            phonebook[0, 0] = "Иван";
+            phonebook[0, 1] = "+7 (111) 111-11-11";
+            phonebook[1, 0] = "Василий";
+            phonebook[1, 1] = "+7 (222) 222-22-22";
+            phonebook[2, 0] = "Тамара";
+            phonebook[2, 1] = "tamara93@gmail.com";
+            phonebook[3, 0] = "Петр";
+            phonebook[3, 1] = "+7 (333) 333-33-33";
+            phonebook[4, 0] = "Алексей";
+            phonebook[4, 1] = "alex0001@gmail.com";
+            Console.WriteLine("Имя\t\tТел\\email");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(phonebook[i, 0] + "\t\t" + phonebook[i, 1]);
+            }
+        }
+    }
+}
---
+++ Lesson3/Task3/Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово для инверсии");
+            string word = Console.ReadLine();
+            if (word.Length == 0)
+            {
+                Console.WriteLine("Вы ничего не ввели");
+                return;
+            }
+            int word_len = word.Length;
+            for (int i = word_len - 1; i >= 0; i--)
+                Console.Write(word[i]);
+
+            Console.WriteLine();
+        }
+    }
+}
