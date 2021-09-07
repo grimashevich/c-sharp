e3d56e081e9debe3dfa0465a027dc2eaf2eddf5a
grimashevich <k.grimashevich@gmail.com>
2021-09-07 21:01:11 +0900

    Task 1

---
+++ Lesson4/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetFullName("Иван", "Иванов", "Петрович"));
+            Console.WriteLine(GetFullName("Петр", "Петров", "Иванович"));
+            Console.WriteLine(GetFullName("Валентин", "Сидоров", "Михайлович"));
+        }
+
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            return $"{lastName} {firstName} {patronymic}";
+        }
+    }
+}
