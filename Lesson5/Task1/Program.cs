158720d75ee17a77d1ace172a153fa7f543fc25f
grimashevich <k.grimashevich@gmail.com>
2021-09-08 21:12:03 +0900

    Task 1

---
+++ Lesson5/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿/*1. Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.*/
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = "../../../task1.txt";
+            Console.WriteLine("Введите данные для сохранения в файл, для окончания наберите q!");
+            if (File.Exists(fileName)) File.Delete(fileName);
+            var firstLine = true;
+            while (true)
+            {
+                var userInput = Console.ReadLine();
+                if (userInput == "q!")
+                    break;
+                if (firstLine) firstLine = false;
+                else File.AppendAllText(fileName, "\n");
+                File.AppendAllText(fileName, userInput);
+            }
+        }
+    }
+}
