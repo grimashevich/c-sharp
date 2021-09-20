139c91453840c62418516c320f972a6eacc6705c
grimashevich <k.grimashevich@gmail.com>
2021-09-20 23:37:52 +0900

    Task 1, 2
    Скомпилировать измененную задачу при помощи ilasm получилось, но приложение не запускается. Требует каких-то зависимостей.

---
+++ Lesson7/Task1_patched/Program.cs
@@ -0,0 +1,19 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Task1.Program
+// Assembly: Task1, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: DAFE99A5-F9B0-4515-B32B-F5249B38515E
+// Assembly location: D:\git_repo\c-sharp\Lesson7\Task1\Task1\bin\Debug\net5.0\Task1.dll
+
+using System;
+
+namespace Task1
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      Console.WriteLine(string.Format("So, x + y = {0}", (object) (3 * 7)));
+      Console.ReadLine();
+    }
+  }
+}
